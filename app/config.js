da4feb18453f356c05f1f2fca0804ad9c6d7f18a
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2023-02-06 17:38:22 +0700

    config

--- app/config.js
+++ app/config.js
@@ -4,11 +4,5 @@ dotenv.config();
 
 module.exports = {
   rootPath: path.resolve(__dirname, ".."),
-  serviceName: process.env.SERVICE_NAME,
-  secretKey: process.env.SECRET_KEY,
-  dbHost: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
-  dbUser: process.env.DB_USER,
-  dbPass: process.env.DB_PASS,
-  dbName: process.env.DB_NAME,
+  secretKey: "fahri",
 };
--- database/index.js
+++ database/index.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const { dbHost, dbPass, dbPort, dbName, dbUser } = require("../app/config");
-// const url = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
 const url = "mongodb+srv://fahri:tfiVdvnoT9qOaWUl@cluster0.f4hrube.mongodb.net/eduworkstore?retryWrites=true&w=majority";
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
